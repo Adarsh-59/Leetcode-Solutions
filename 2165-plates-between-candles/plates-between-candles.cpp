ee582e95c2af932f794b841cdac70022894313b8
Adarsh K Gupta <85187939+Adarsh-59@users.noreply.github.com>
2025-09-02 23:10:15 +0530

    Time: 10 ms (82.12%) | Memory: 146.9 MB (74.51%) - LeetSync

--- 2165-plates-between-candles/plates-between-candles.cpp
+++ 2165-plates-between-candles/plates-between-candles.cpp
@@ -12,7 +12,24 @@ public:
         for(int i=1 ; i<n ; i++){
             preCount[i] = s[i] == '|' ? i : preCount[i-1];
             postCount[n-1-i] = s[n-1-i] == '|' ? n-1-i : postCount[n-i];
+            // if(s[i] == '|'){
+            //     preCount[i] = i;
+            // }
+            // else{
+            //     preCount[i] = preCount[i-1];
+            // }
         }
+
+        // postCount[n-1] = n-1;
+        // for(int i=n-2 ; i>=0 ; i--){
+        //     postCount[i] = s[i] == '|' ? i : postCount[i+1];
+        //     // if(s[i] == '|'){
+        //     //     postCount[i] = i;
+        //     // }
+        //     // else{
+        //     //     postCount[i] = postCount[i+1];
+        //     // }
+        // }
 
         int lstIdx = 0;
         bool candleFound = s[0] == '|' ? 1 : 0;
@@ -34,11 +51,36 @@ public:
             }
         }
 
-        for(int i=0 ; i<m ; i++){
-            int lftIdx = postCount[queries[i][0]];
-            int rghtIdx = preCount[queries[i][1]];
+        // for(int i=0 ; i<n ; i++){
+        //     cout << i << " ";
+        // }
+        // cout << endl;
+        // for(int i=0 ; i<n ; i++){
+        //     cout << preCount[i] << " ";
+        // }
+        // cout << endl;
+        // for(int i=0 ; i<n ; i++){
+        //     cout << postCount[i] << " ";
+        // }
+        // cout << endl;
+        // for(int i=0 ; i<n ; i++){
+        //     cout << plates[i] << " ";
+        // }
+        // cout << endl;
 
-            ans[i] = lftIdx<rghtIdx ? plates[rghtIdx] - plates[lftIdx] : 0;
+        for(int i=0 ; i<m ; i++){
+            int left = queries[i][0];
+            int right = queries[i][1];
+
+            int lftIdx = postCount[left];
+            int rghtIdx = preCount[right];
+
+            if(lftIdx >= rghtIdx){
+                ans[i] = 0;
+            }
+            else{
+                ans[i] = plates[rghtIdx] - plates[lftIdx];
+            }
         }
 
         return ans;
