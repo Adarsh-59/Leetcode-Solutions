51d2e41f3c428ecbd407f84c8af46885b9e7ead2
Adarsh K Gupta <85187939+Adarsh-59@users.noreply.github.com>
2025-09-03 23:55:21 +0530

    Time: 1026 ms (5.69%) | Memory: 19.5 MB (79.36%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n, cnt, l;
-        n = nums.size();
-        cnt = 0;
-        l = 0;
-        for(int i=1 ; i<n ; i++){
-            if(nums[i] == nums[l]){
-                if(i-l > 1){
-                    cnt++;
-                    auto it = nums.begin()+i;
-                    nums.erase(it);
-                    i--;
-                    n--;
+        int n = nums.size();
+        int currCnt = 0, lastIdx = n-1;
+        int ans = 0;
+        for(int l=0, r=0 ; r<=lastIdx ; ){
+            cout << currCnt << " " << l << " " << r << endl;
+            if(currCnt < 2){
+                if(nums[l] == nums[r]){
+                    r++;
+                    currCnt++;
+                }
+                else{
+                    l = r;
+                    currCnt = 0;
                 }
             }
-            else{
-                l = i;
+            else if(currCnt==2){
+                if(nums[l] == nums[r]){
+                    int idx = r;
+
+                    while(idx < lastIdx){
+                        swap(nums[idx], nums[idx+1]);
+                        idx++;
+                    }
+
+                    ans++;
+                    lastIdx--;
+                }
+                else{
+                    l = r;
+                    currCnt = 0;
+                }
             }
         }
 
-        return n;
+        cout << endl;
+        for(int i=0 ; i<n ; i++){
+            cout << nums[i] << " ";
+        }
+        cout << endl << lastIdx+1;
+        return lastIdx+1;
     }
 };
