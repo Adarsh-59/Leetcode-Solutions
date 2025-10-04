98ae4aab1b9fdec6e1ff09041b0ecfb03e6a1c62
Adarsh K Gupta <85187939+Adarsh-59@users.noreply.github.com>
2025-10-04 23:26:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (72.76%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n < 10){
+            if(n == 1 || n == 7){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            int sum = 0;
+            while(n){
+                int r = n%10;
+                sum += r*r;
+                n/=10;
+            }
+
+            return isHappy(sum);
+        }
+    }
+};
