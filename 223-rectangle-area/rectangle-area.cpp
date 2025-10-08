72ec571192a2384c5e80f47d8af308949815ea60
Adarsh K Gupta <85187939+Adarsh-59@users.noreply.github.com>
2025-10-09 00:13:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (40.81%) - LeetSync

---
+++ 223-rectangle-area/rectangle-area.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        if(ax2 >= bx1 && ax2 <= bx2 || bx2 >= ax1 && bx2 <= ax2){
+            if(ay2 >= by1 && ay2 <= by2 || by2 >= ay1 && by2 <= ay2){
+                return (ax2-ax1)*(ay2-ay1)+(bx2-bx1)*(by2-by1) - (min(ax2, bx2)-max(ax1, bx1))*(min(ay2, by2)-max(ay1, by1));
+            }
+            else{
+                return (ax2-ax1)*(ay2-ay1)+(bx2-bx1)*(by2-by1);
+            }
+        }
+        else{
+            return (ax2-ax1)*(ay2-ay1)+(bx2-bx1)*(by2-by1);
+        }
+        
+    }
+};
