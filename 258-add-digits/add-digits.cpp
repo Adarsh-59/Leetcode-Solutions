c280742d6467787b099d535549ae81082e7eefef
Adarsh K Gupta <85187939+Adarsh-59@users.noreply.github.com>
2025-10-14 22:50:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (8.82%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int sum(int n){
+        int s = 0;
+        
+        while(n){
+            s += n%10;
+            n /= 10;
+        }
+        
+        return s;
+    }
+    
+    int addDigits(int num) {
+        int s, n;
+        n = num;
+        
+        do{
+            s = sum(n);
+            n = s;
+        }while(s>9);
+        
+        return s;
+        
+    }
+};
